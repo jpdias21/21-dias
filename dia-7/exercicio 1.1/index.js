435e4a1cadfaa4f2dcd2ce1fb09818192908e7b0
Joao Pedro <joaopedrodiasde@gmail.com>
2024-08-13 13:48:45 -0300

    Adicionando pasta dia-7

--- dia-7/exercicio 1.1/index.js
+++ dia-7/exercicio 1.1/index.js
@@ -1,21 +1,26 @@
-let valor = 1000 
+let condicao = true
+valor = 1000 
 
-do{ let nome = prompt('Digite seu nome :')
-    let cpf = Number(prompt('Digite seu CPF :'))
-    let opcao = prompt('Escolha a opcão entre \n 1. Saque \n 2. Deposito')
+do{
+    let nome = prompt('digite seu nome')
+    let cpf = parseInt(prompt('Digite seu CPF: '))
+    let opcao = prompt('Escolha a opção que voce ira vfazer \n1. Saque \n2. Deposito')
 
-        if(opcao === '1'){
-            let saque = Number(prompt('Escolha qual o valor voce irá sacar'))
-                if(saque <= valor){
-                    console.log('Opa ' + nome + 'voce sacou ' + (saque) + ' Reais e sobrou ' + (valor - saque + ' Reais da sua conta'))
-                }else if(saque >= valor){
-                    console.log(nome + ' voce esta tentando sacar um valor maior do que esta na sua conta')
-                }
-        }else if(opcao === '2'){
-                    let deposito = Number(prompt('Digite o valor que voce quer depositar'))
-                    console.log('Voce acabou de adicionar ' + deposito + 'Reais na sua conta com e total de :' + (valor + deposito) + ' Reais na sua conta bancaria')
-                }
-                
-                let continuar = prompt('Voce quer continuar \n 1. Quero continuar \n 2. Quero parar')
+    if(opcao === '1'){
+        let valorDeSaque = Number(prompt('Digite o valor que voce ira sacar :'))
+        if(valorDeSaque <= valor){
+            console.log('Voce sacou ' + valorDeSaque + ' Reais da sua conta e sobrou ' + (valor - valorDeSaque) + ' Reais na sua conta')
+        }else if( valorDeSaque > valor){
+            console.log('Voce esta tentando sacar um valor maior que voce tem na sua conta')
+        }
+    }else if(opcao === '2'){
+        let valorDeposito = Number(prompt('Digite o valor que voce ira depositar :'))
+        console.log('Voce depositou ' + valorDeposito +' Reais na sua conta e o total ficou ' + (valor + valorDeposito) + ' Reais')
+    }
+    let continuar = prompt('Voce deseja continuar \n1.Não \n2.Sim')
+    if(continuar === '1'){
+        condicao = false
+        console.log('Voce escolheu parar por aqui')
+    }
 
-    } while(continuar === '1')
+}while(condicao )
