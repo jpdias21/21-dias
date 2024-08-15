ffa84fff690cd93486bea04b68aa29225cf1e9a1
Joao Pedro <joaopedrodiasde@gmail.com>
2024-08-15 16:06:02 -0300

    Adicionando pasta dia-9

--- dia-9/index.js
+++ dia-9/index.js
@@ -1,51 +1,51 @@
-
 let condicao = true
 
 do{
 let nome = prompt('Insira seu nome :')
-let idade = Number(prompt('Insira sua idade:'))
+let idade = Number(prompt('insira sua idade :'))
 let peso = parseInt(prompt('Insira seu peso :'))
 let altura = Number(prompt('Insira sua altura :'))
-let profissao = prompt('Insira qual e a sua profissão :')
-let exibe = 'Olá ' + nome + ', você tem ' + idade + 'Anos, é ' + profissao + ', tem ' + altura + 'M de altura e pesa ' + idade +' Kg'
-console.log(exibe)
-
-let idadeEmMesses = idade * 12
-let idadeEmsamanas = idade * 52
-let idadeEmDias = idade * 365 
-
-console.log('Sua idade em messes :' + idadeEmMesses)
-console.log('Sua idade em semanas :' + idadeEmsamanas)
-console.log('Sau idade em dias :' + idadeEmDias)
+let profissao = prompt('insira a sua profissão :')
+let texto = ('Olá '+ nome + ', você tem ' + idade + ' anos, é ' + profissao + ', tem ' + altura + 'M de altura e pesa ' + peso + 'KG .')
+console.log(texto)
 
 if(idade >= 18){
-    console.log('Esta liberado para tomar umas geladas')}else if(idade <= 18 ){
-        console.log('Sem gelada para voce')
-    }
-
-let imc = (peso /(altura * altura))
-console.log(imc)
-if(imc < 18.5){
-    console.log('Voce esta em um nivel de magreza')
-}else if(imc > 18.5 && imc < 24.9 ){
-    console.log('voce se encontra no nivel normal')
-}else if(imc > 24.9 && imc < 30){
-    console.log('Voce esta com sobrepeso')
-}else if(imc > 30){
-    console.log('Voce esta em estado de obesidade')
+    console.log('Esta leberado para tomar umas geladas')
+}else if(idade < 18){
+    console.log('Sem gelada para você.')
 }
-let idadeDeNascimento = (2024 - idade)
-console.log('O ' + nome + ' Nasceu em ' + idadeDeNascimento)
 
-for(let i = (2024 - idade) ; i <= 2024; i++ ){
+let idadeEmDias = idade * 365
+let idadeEmSemanas = idade * 54
+let idadeEmMes = idade * 12
 
-    console.log(i + '- ' + (i - idadeDeNascimento) + ' Anos de idade')}
-    
-    let continuar = prompt('Voce quer iserir novos dados ou para por aqui \n1.Continuar \n2.Parar')
-        if(continuar === '2'){
-            condicao = false
-        }
-    
+console.log('você viveu ' + idadeEmDias + ' dias')
+console.log('Voce viveu ' + idadeEmSemanas + ' semanas')
+console.log('Voce viveu ' + idadeEmMes + ' meses')
 
+let imc = peso / (altura * altura)
+console.log('Seu imc e ' + imc)
+if(imc <= 18.5){
+    console.log('Voce esta em um nivel e magreza')
+}else if(imc > 18.5 && imc <= 24.9){
+    console.log('Voce esta em um nivel normal')
+}else if(imc > 24.9 && imc <= 30){
+    console.log('Voce esta em um nivel de sobrepeso')
+}else if(imc > 30){
+    console.log('Voce esta em um nivel de obesidade')
+}
+
+let anoDdeNascimento = 2024 - idade
+console.log('Voce nasceu em ' + anoDdeNascimento)
+
+for(let num1 = anoDdeNascimento ; num1 <= 2024 ; num1++ ){
+    let idadeAtual = num1 - anoDdeNascimento
+    console.log(num1 + ' - ' + idadeAtual + '   Anos')
+}
+
+    let continuar = prompt('voce deseja inserir novos dados ou finalizar. \n1.Continuar \n2.Finalizar por aqui')
+    if(continuar === '2'){
+        console.log('Você escolheu para por aqui')
+        condicao = false
+    }
 }while(condicao)
-
