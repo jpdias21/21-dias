f2ed0de0d0aa522258280c47c241f7f5b7c42cdb
Joao Pedro <joaopedrodiasde@gmail.com>
2024-08-20 17:39:45 -0300

    adicionando pasta dia-10 pasta 2, não acabei ainda

---
+++ dia-10/2/index.js
@@ -0,0 +1,8 @@
+let numero = []
+
+for(let i = 0 ; i <= 10 ; i++){
+    let num1 = parseInt(prompt('Digite um algum numero positivo :'))
+    for(let num2 = -1 ; num2 <= num1 ; num2 += num2)
+    numero.push(num1)
+}
+console.log(num1)
