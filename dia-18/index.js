46211326f7515230a411411a708323c8f249531b
Joao Pedro <joaopedrodiasde@gmail.com>
2024-10-03 16:21:06 -0300

    Adicionando pasta dia-18

--- dia-18/index.js
+++ dia-18/index.js
@@ -1,139 +1,99 @@
-
-arrayLivros = []
+arrayLivro = []
 arrayBiblioteca = []
 
-class Livro{
+class Livro {
     Titulo
     Autor
     Editora
     AnoDePublicacao
-    disponibilidadeDoLivro = true
-    constructor(
-        Titulo , Autor, Editora, AnoDePublicacao){
+    Disponibilidade = true
+    constructor(Titulo, Autor, Editora,  AnoDePublicacao, Disponibilidade = true){
         this.Titulo = Titulo
         this.Autor = Autor
         this.Editora = Editora
         this.AnoDePublicacao = AnoDePublicacao
+        this.Disponibilidade = Disponibilidade
+       
     }
 }
 
-/// ------------------------
-arrayLivros.push(new Livro ('O rei', 'Joao', 'Editora = 1', 2002))
-arrayLivros.push(new Livro ('Joao', 'Maria', 'Editora = 2', 1990))
-arrayLivros.push(new Livro ('o ', 'Pedro', 'Editora = 3', 1998))
-arrayLivros.push(new Livro ('48 Leis do poder', 'Dias', 'Editora = 4', 2010))
-/// ------------------------------
+let cadastraLivro1 = new Livro ('Titulo do Livro 01', 'Autor do livro 01', 'Nome da editora 01', parseInt(11938301))
+let cadastraLivro2 = new Livro ('Titulo do Livro 02 ', 'Autor do livro 02', 'Nome da editora 02', parseInt(11938302))
+let cadastraLivro3 = new Livro ('Titulo do Livro 03', 'Autor do livro 03', 'Nome da editora 03', parseInt(11938303), false )
 
-class Biblioteca{
+arrayLivro.push(cadastraLivro1)
+arrayLivro.push(cadastraLivro2)
+arrayLivro.push(cadastraLivro3)
+
+class Biblioteca {
     Nome 
     Endereco
     Telefone
     AcervoDeLivros = []
-    constructor(Nome,Endereco,Telefone){
+    constructor(Nome, Endereco, Telefone, AcervoDeLivros ){
         this.Nome = Nome
         this.Endereco = Endereco
         this.Telefone = Telefone
-        this.AcervoDeLivros = acervo 
-    }
-}
-
-
-
-let acervo = ['livro 1' , 'livro 2', 'Livro 3']
-
-arrayBiblioteca.push(new Biblioteca('Joao 1' , 'Rua Maria 1' , parseInt('11924323452334'),))
-arrayBiblioteca.push(new Biblioteca('Joao 2' , 'Rua Maria 2' , parseInt('11234523454534'),))
-arrayBiblioteca.push(new Biblioteca('Joao 3' , 'Rua Maria 3' , parseInt('1192234334533'), ))
-
-function BuscarLivro(){
-
-    let buscarPeloNomeDoLivro = prompt('Digite o nome do Livro ')
-    let achouLivro = arrayLivros.find(l => l.Titulo === buscarPeloNomeDoLivro)
-    if(achouLivro){
-        console.log(`Titulo : ${achouLivro.Titulo}`)
-        console.log(`Autor : ${achouLivro.Autor}`)
-        console.log(`Editora : ${achouLivro.Editora}`)
-        console.log(`Ano de publicação : ${achouLivro.AnoDePublicacao}`)
-        console.log(`Disponivel : ${achouLivro.disponibilidadeDoLivro}`)
-    }else{
-        console.log('Não tem nenhum livro com este nome ou voce digitou errado')
-    }
-}
-
-function PegarLivroEmprestado(){
-    let buscarLivro = prompt('Digite o nome do Livro ')
-    let achouLivro = arrayLivros.find(l => l.Titulo === buscarLivro)
-    if(achouLivro){
-
-        if(achouLivro.disponibilidadeDoLivro === true){
-            let livroDisponivel = prompt(`O livro ${achouLivro.Titulo} esta disponivel, Digite \n1.para pegar  \n2.para sair`)
-            if(livroDisponivel === '1' ){
-                achouLivro.disponibilidadeDoLivro = false ;
-                
-                console.log(`Voce pegou o livro  ${achouLivro.Titulo}, e ele esta indisponivel.`)
-
-            } else if(livroDisponivel === '2'){
-                console.log('Voce escolheu sair')
-                return
-        }else{
-            console.log('este livro não esta disponivel')
-        }
+        this.AcervoDeLivros = AcervoDeLivros 
         
-    }else{
-        console.log('Não tem livro com este nome ou voce colocou o nome errado')
     }
-}
-}
 
-function DevolverLivro(){
-    let nomeDoLivro = prompt('Digite o nome do livro :')
-    achouLivro = arrayLivros.find(l => l.Titulo === nomeDoLivro)
-    if(achouLivro){
-        if(achouLivro.disponibilidadeDoLivro === true){
-            console.log('Este livro ja esta dispovivel')
-            return
-        }else if(achouLivro.disponibilidadeDoLivro === false){
-            let devolverLivro = prompt('Digite \n1.Para devolver \n2.Voltar e não devolver')
-            if(devolverLivro === '1'){
-            achouLivro.disponibilidadeDoLivro = true
-            console.log(`Agora o livro ${achouLivro.Titulo} esta disponivel`)
-        }else if(devolverLivro === '2'){
-            console.log('Voce não devolveu o livro e esolheu voltar')
-            return
+        BuscarOlivroPeloTitulo(titulo){
+            let achouTitulo = arrayLivro.find(t=> t.Titulo === titulo)
+            if(achouTitulo){
+                console.log(`Nome do Livro : ${achouTitulo.Titulo}`)
+                console.log(`Autor do titulo : ${achouTitulo.Autor}`)
+                console.log(`Editora : ${achouTitulo.Editora}`)
+                console.log(`Ano de publição : ${achouTitulo.AnoDePublicacao}`)
+            }else{
+                console.log('Não tem nenhum livro com este nome')
+            }
         }
-        }else{
-            console.log('no acervo da biblioteca não tem nenhum livro com este nome para adicionar')
-            return
+    
+        PegarLivroEmprestado(titulo){
+            let achouTitulo = arrayLivro.find(t => t.Titulo === titulo)
+            if(achouTitulo){
+                if(achouTitulo.Disponibilidade === true){
+                    console.log('Este livro esta disponivel')
+                }else{
+                    console.log('Este livro esta indisponivel')
+                }
+            }else{
+                console.log('Não tem livro com esta titulo, ou voce errou na digitação')
+            }
+        } 
+
+        DevolverLivro(titulo){
+            let achouTitulo = arrayLivro.find(t => t.Titulo === titulo)
+            if(achouTitulo){
+                if(achouTitulo.Disponibilidade === false){
+                    console.log(`Voce esta devolvendo o livro ${achouTitulo.Titulo}`)
+                    achouTitulo.Disponibilidade = true
+                }else{
+                    console.log(`Este livro esta disponivel`)
+                }
+            }else{
+                console.log('Não tem nenhum livro com este titulo ou voce digitou errado.')
+            }
         }
-    }
-}
+}///TERMINA
 
+let acervo1 = 'livro 1'
+let acervo2 = 'livro 2'
+let acervo3 = 'livro 3'
 
-let continuar = true
+let cadastraBiblioteca1 = new Biblioteca ('Nome da biblioteca', 'Endereço',  parseInt(11938301) , acervo1)
+let cadastraBiblioteca2 = new Biblioteca ('Titulo do Livro 02 ', 'Autor do livro 02', parseInt(11938302), acervo2 )
+let cadastraBiblioteca3 = new Biblioteca ('Titulo do Livro 03', 'Autor do livro 03', parseInt(11938303), acervo3)
 
-do{
-let questao = prompt('\n1.Buscar o livro \n2.Pegar livro emprestado \n3.Devolver Livro \n4.Encerrar')
-switch(questao){
-    case '1':
-        BuscarLivro()
-    break
+arrayBiblioteca.push(cadastraBiblioteca1)
+arrayBiblioteca.push(cadastraBiblioteca2)
+arrayBiblioteca.push(cadastraBiblioteca3)
 
-    case '2':
-        PegarLivroEmprestado()
-    break
-    case '3' :
-        DevolverLivro()
-    break
-    case '4':
-        continuar = false
-        console.log(arrayBiblioteca)
-        console.log(arrayLivros)
-        break
-
-    default :
-    console.log('erro')
-
-    break
-
-} 
-}while(continuar)
+///
+cadastraBiblioteca1.BuscarOlivroPeloTitulo('Titulo do Livro 01')
+cadastraBiblioteca1.PegarLivroEmprestado('Titulo do Livro 01')
+cadastraBiblioteca3.DevolverLivro('Titulo do Livro 03')
+/////
+console.log(arrayLivro)
+console.log(arrayBiblioteca)
