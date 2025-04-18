ce5311b9ec425e7e29182c5f9ad4b4eb6a3f9183
Joao Pedro <joaopedrodiasde@gmail.com>
2025-04-18 16:11:53 -0300

    18/04/2024 2:30/6 estudadando mais sobre o Sass style e agora ireimos palicar no desafio

--- React/16-SCSS-STYLE/src/pages/Home.tsx
+++ React/16-SCSS-STYLE/src/pages/Home.tsx
@@ -3,10 +3,11 @@ import './Home.scss'
 
 function Home() {
   
+
   return (
-    <div className="container">
-    Estilo com Sass funcionando!
-  </div>
+    <>
+     <p>Joao Pedro</p>
+    </>
   )
 }
 
