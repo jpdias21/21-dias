eaab04817a7034dbff25d85857e60e1d6e34c962
Joao Pedro <joaopedrodiasde@gmail.com>
2025-04-29 17:23:06 -0300

    29/04/20204 6/6 terminei o desafio do formulario e agora estou estudando sobre o formulario com formik

--- React/18-React-Formularios/src/pages/Home.tsx
+++ React/18-React-Formularios/src/pages/Home.tsx
@@ -1,40 +1,57 @@
 import { useEffect, useState } from 'react'
 import './Home.scss'
+import axios from 'axios'
 
 function Home() {
+  const [produto, setProduto] = useState({nome : '', marca : '', valor: '', categoria: ''})
+  const [adicionado, setAdicionado] = useState<boolean>(false)
+
   
-  const [formValues, setFormValues] = useState({name : '', age: ''})
-  const [disable , setDisable] = useState(true)
-
-  const handleSubmit = (event : any) => {
+  const handleSubmit = async (event : any) => {
     event.preventDefault()
-    console.log('clicou')
-    console.log(formValues)
-    setDisable(true)
-    setFormValues({name : '', age: ''})
-    
-  }
-
-  const handleChange = (event : React.ChangeEvent<HTMLInputElement>) => {
-    setFormValues({...formValues, [event.target.name] :event.target.value })
-  }
-
-  useEffect(() => {
-    if(formValues.name && formValues.age){
-      setDisable(false)
+    try {
+      const response = await axios.post('https://apigenerator.dronahq.com/api/UT5bX06g/data', {... produto, nome : produto.nome , marca : produto.marca, valor: Number(produto.valor), categoria: produto.categoria })
+      console.log('response', response.data)
+      setProduto({ nome: '', marca: '', valor: '', categoria: '' })
+      setAdicionado(true)
+    } catch (error) {
+      console.error('error', error)
+      setAdicionado(false)
     }
-  }, [handleSubmit])
+  }
+
   return (
     <>
-    <h2>Formulario</h2>
-   <form >
-    <input type="text" name='name' value={formValues.name} onChange={handleChange} placeholder='Digete seu nome'/>
+  
+  <form  onSubmit={handleSubmit}>
+    <label htmlFor="NomeProduto">Nome do Produto : </label>
+    <input type="text" id='NomeProduto' value={produto.nome} placeholder='Digite o nome' required onChange={(event) => setProduto({...produto, nome: event.target.value}) }/>
     <br />
-    <input type="text" name='age' value={formValues.age} onChange={handleChange} placeholder='Digite sua idade' />
+    <label htmlFor="marca_produto">Marca : </label>
+    <input type="text" id='marca_produto' value={produto.marca} placeholder='Digite a marca' required onChange={(event) => setProduto({...produto, marca :event.target.value})} />
+    <br />
+    <label htmlFor="valor_produto">Valor do produto : </label>
+    <input type="number" id='valor_produto' value={produto.valor} placeholder='Digite o valor' required onChange={(event) => setProduto({...produto, valor: event.target.value})}/>
+    <br />
+    <legend>Categoria do produto :</legend>
+    
+    <input type="radio" id='alimenticio' name='categoria_produto' value='alimenticio'
+    onChange={(event) => setProduto({...produto, categoria: event.target.value})}/><label htmlFor="alimenticio">alimenticio </label>
+    
+    <input type="radio" id='limpeza' name='categoria_produto' value='limpeza' required onChange={(event) => setProduto({...produto, categoria: event.target.value})} /><label htmlFor="limpeza">Limpeza </label>
+
+    <input type="radio" id='Higiene_Pessoal' name='categoria_produto' value='Higiene_Pessoal' onChange={(event) => setProduto({...produto, categoria: event.target.value})} /><label htmlFor="Higiene_Pessoal">Higiene Pessoal </label>
+    
+    <input type="radio" id='Hortifruti' name='categoria_produto' value='Hortifruti' onChange={(event) => setProduto({...produto, categoria: event.target.value})} /><label htmlFor="Hortifruti">Hortifruti </label>
+    
+    <input type="radio" id='bebidas' name='categoria_produto' value='bebidas' onChange={(event) => setProduto({...produto, categoria: event.target.value})} /><label htmlFor="bebidas">Bebidas</label>
     <br />
     <br />
-    <button onClick={handleSubmit} disabled={disable}>Enviar</button>
-   </form>
+    <button>Enviar Produto</button>
+    <br />
+  </form>
+  <br />
+  {adicionado ? <p>Produto adicionado com sucesso</p> : '' }
     </>
   )
 }
